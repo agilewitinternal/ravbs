0b2f8917412f93d0970705f97844cd85f04e0977
SiddAgilewit <siddhu@agilewits.com>
2024-06-05 17:55:27 +0530

    DropDownContent

---
+++ src/constant/DropDownContent.js
@@ -0,0 +1,18 @@
+export const DropDownContent={
+
+    Expolre:"EXPLORE",
+    Service:"SERVICES",
+    Product:"PRODUCT",
+    AboutUS:"ABOUT US",
+    CurrentOpening:"CURRENT OPENING",
+    Consulting:"COUNSULTING",
+    AppDevelopment:"APP DEVELOPMENT",
+    Training:"TRAINING",
+    Consen:"CONSEN",
+    Letsworktogether:"LET'S WORK TOGETHER +"
+
+
+}
+
+
+
