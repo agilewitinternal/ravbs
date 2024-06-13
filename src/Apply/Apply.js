0ea38fa672cd0dc4890e9650a9fb9dfdd946b119
SiddAgilewit <siddhu@agilewits.com>
2024-06-13 20:37:53 +0530

    ApplicationPage

--- src/Apply/Apply.js
+++ src/Apply/Apply.js
@@ -4,46 +4,59 @@ import DesktopHeader from '../DeskTopHeader/DeskTopHeader'
 import ServiceHeaders from '../ServiceHeaders/ServiceHeaders'
 import BottomPage from '../BottomPage/BottomPage'
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 import "./Apply.css"
 const Apply = () => {
+    const navigate = useNavigate();
+   
     const [applicantFirstName, setApplicantFirstName] = useState("")
     const [applicantLastName, setApplicantLastName] = useState("")
     const [applicantContact, setApplicantContact] = useState("")
     const [applicantMail, setApplicantMail] = useState("")
-    const[resume,setResume]=useState("")
+    const [resume, setResume] = useState("")
+    const[experiance,setExperiance]=useState("")
+    const[noticePeriod,setNoticePeriod]=useState("")
     const [SuccessMessage, SetSuccessMessage] = useState("")
 
-    const UpdateResume=(e)=>{
-        setResume(e.target.files[0])
-    }
+    const UpdateResume = (e) => {
+       
+
+        const file = e.target.files[0];
+
+        if (file) {
+       
+          const reader = new FileReader();
+          reader.onload = function(event) {
+          
+            setResume(event.target.result);
+          };
+          reader.readAsDataURL(file);
+        }
     
+    }
+
     const UploadDetails = async (event) => {
         event.preventDefault();
         const URL = "https://agilewitapplicantdetails-default-rtdb.firebaseio.com/.json";
-        
-        const formData = new FormData();
-       
-        formData.append('applicantFirstName', applicantFirstName);
-        formData.append('applicantLastName', applicantLastName);
-        formData.append('applicantContact', applicantContact);
-        formData.append('applicantMail', applicantMail);
-        formData.append('resume', resume);
-      
+
+     
+
         try {
-            const response = await axios.post(URL, resume);
-        console.log(response)
+            const response = await axios.post(URL, {applicantFirstName,applicantLastName,applicantContact,applicantMail,resume,experiance,noticePeriod});
+            console.log(response)
             SetSuccessMessage("Application Sent successfully");
             setApplicantFirstName("");
             setApplicantLastName("");
             setApplicantContact("");
             setApplicantMail("");
             setResume("");
+            navigate("/");
         } catch (error) {
             console.error('Error uploading details:', error);
-            
+
         }
     };
-    
+
 
     return (
         <div className='HomeTopLayer'>
@@ -63,11 +76,24 @@ const Apply = () => {
                         <input type="text" value={applicantContact} placeholder="Contact Details" className="Email" name="number" pattern="[0-9]*" onChange={(e) => { setApplicantContact(e.target.value) }} />
 
                         <input type="text" value={applicantMail} placeholder="E-mail" pattern=".*@.*" className="Email" name="from_name" onChange={(e) => { setApplicantMail(e.target.value) }} />
-                        <label>Upload Resume</label>
-                        <input type='file' className='file-input' onChange={UpdateResume}  />
+                        <select className="Email" onChange={(e)=>setExperiance(e.target.value)}>
+                            <option selected>Experiance</option>
+                            <option value=" Fresher">Fresher</option>
+                            <option value=" 0-2 Years">0-2 Years</option>
+                            <option value="2-4Years">2-4Years</option>
+                            <option value="4-6Years">4-6Years</option>
+                            <option value="6-8Years">6-8Years</option>
+                        </select>
 
-
-
+                        <select className="Email" onChange={(e)=>{setNoticePeriod(e.target.value)}}>
+                            <option>Notice Period</option>
+                            <option>Immediate Jointer</option>
+                            <option>1 Month</option>
+                            <option>2 Month</option>
+                            <option>3 Month</option>
+                        </select>
+                       
+                        <input type='file' className='file-input' onChange={UpdateResume} />
                         <button style={{ opacity: (applicantFirstName === "" || applicantLastName === "" || applicantContact === "" || applicantMail === "") ? 0.2 : 1 }} type="submit">Send Application</button>
                         <p className="SuccessMessage">{SuccessMessage}</p>
                     </form>
