66cc7e7f21fe8ec12de5dda34702cfba9c27ae17
SiddAgilewit <siddhu@agilewits.com>
2024-06-18 21:54:42 +0530

    Updates

--- src/constant/Home.js
+++ src/constant/Home.js
@@ -41,7 +41,7 @@ export const Partners = [
         PartnerName: "Akshara",
         PartnerIcon: Akshara,
         PartnerCompany: "Director Of SK Instistution",
-        PartnerOpinion: "“Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.”"
+        PartnerOpinion: "“Lorem Ipsum is a simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.”"
     }
 ]
 
