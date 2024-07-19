20858831f978873fca88c5551da50e9795ae398d
SiddAgilewit <siddhu@agilewits.com>
2024-07-20 00:48:24 +0530

    OTP Update

--- src/ResetPassword/ResetPassword.js
+++ src/ResetPassword/ResetPassword.js
@@ -1,41 +1,48 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 import DesktopHeader from '../DeskTopHeader/DeskTopHeader';
 import ServiceHeaders from '../ServiceHeaders/ServiceHeaders';
 import BottomPage from '../BottomPage/BottomPage';
 import Reset from '../Assets/Reset.png';
+import { signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
+import auth  from '../ResetPassword/Setup';
 import './ResetPassword.css';
 
-
 const ResetPassword = () => {
+    const [phoneNumbers, setPhoneNumber] = useState("");
+
+
+    const handleReset = async (e) => {
+        e.preventDefault();
+
+        try {
+            const phoneNumberWithCountryCode = `+91${phoneNumbers}`;
+            const recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha",{});
+            const confirmation = await signInWithPhoneNumber(auth, phoneNumberWithCountryCode, recaptchaVerifier);
+            console.log(confirmation)
+        } catch (error) {
+            console.error("Error sending password reset email:", error);
+            alert("Error sending password reset email. Please try again.");
+        }
+    };
+
+
     return (
         <div className='HomeTopLayer'>
             <Header />
             <DesktopHeader />
             <div className='SubHomeSecondLayer'>
                 <ServiceHeaders ServiceHeadersInfo="Reset" />
-                <div className="Reset-TopLayer"> 
-            <img className='AdminLogo' src={Reset} alt="AdminLogo" />
-           
-<form className="Forgot">
-    <h3>Reset Your Password</h3>
-<input
-placeholder="Enter Your E-Mail"
-type="email"
-className="Reset-Input"
-required
-/>
-<input
-className="Reset-Input"
-placeholder="Enter Your OTP"
-type="password"
-required
-/>
-<button className="Reset-Button">Submit</button>
-</form>
-            
+                <div className="Reset-TopLayer">
+                    <img className='AdminLogo' src={Reset} alt="AdminLogo" />
+                    <div className="Forgot">
+                        <h3>Reset Your Password</h3>
+                        <input onChange={(e) => setPhoneNumber(e.target.value)} value={phoneNumbers} placeholder="Enter Your Contact Number" pattern="[0-9]" className="input-field" type="tel" name="mobile" maxLength="10" />
+                        <div id="recaptcha"></div>
+
+                        <button className="Reset-Button" onClick={handleReset}>Submit</button>
+                    </div>
                 </div>
-
-
                 <BottomPage />
             </div>
         </div>
