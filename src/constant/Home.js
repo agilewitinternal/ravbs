5fb36c8e42f49de240e6341e8d1f94dd1454c326
SiddAgilewit <siddhu@agilewits.com>
2024-08-14 01:23:52 +0530

    Responcive Updates

--- src/constant/Home.js
+++ src/constant/Home.js
@@ -51,7 +51,7 @@ export const HomePageContent = {
     HomePageMainingHeading: 'Innovate with Our Tech Experts.',
     MobileViewCaption: `"Technology empowers us to dream beyond boundaries and make those dreams a reality"`,
     HomePageTagLine: "Elevate Your Business with Our Technology Consulting and Software Development Expertise",
-    LetsWork: "LET WORK TOGETHER +",
+    LetsWork: "LET'S WORK TOGETHER +",
     ShareYourIdea: `You Share your idea We get it done`,
     IdeaDescription: `Agilewit Solutions is incorporated as a Technology Consulting services and Software product development firm. Head quartered in Texas and Offshore development centers in India, we are involved in creating strategy, implementing, deploying, and supporting a wide range of
     solutions for clients worldwide.`
