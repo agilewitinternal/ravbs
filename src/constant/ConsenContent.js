de1b8bb5293e1fd5309d8d32b99d2dd18208df7d
SiddAgilewit <siddhu@agilewits.com>
2024-08-19 23:51:02 +0530

    Updates

--- src/constant/ConsenContent.js
+++ src/constant/ConsenContent.js
@@ -1,8 +1,8 @@
-
+import uploaddocuments from '../Assets/upload-documents.gif'
 export const ConsenContent={
 
     ConcenTitle:"CONSEN",
-    DocumentUpload:"https://s3-alpha-sig.figma.com/img/4aa7/487b/01791e58b9b94cf5aacfbeaba2cb926c?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WS6i~V6z3lpJW56HVBVWye9zPzcBwe55xgldXyBMA3Fe2o8GAYV-pPhPCJgzrx08CZkS6UoOEmN67Glc7aN91WLhfFnqjBiwI-y8vAIFWd6JhjI~JK2cl8YJxhxt-zf9IW7pLnS2NeQp1-s-9-Bosu-z5uKuhkSBg9kEY~j6-tNnZX-U9eVWhm16XgSbn2SK~JUuOy47AM7oWIAqWPm6FmN2J73PhGySxMvPkkOJDZIp~8HJIstH0G-qvFNwlOwFYfV0M8hrq9u1rKJEgq4TqD9auGq7aTNRMPUTz9Wc7fLI-WV9Ia5KIFYx5PR5mE5hEdpNYwK77dzIlittiJBSzQ__",
+    DocumentUpload:uploaddocuments,
     ConcenDescription:"CONSEN is one of the products of the AGILEWIT SOLUTIONS INC. This application reads the coulmn data provided in excel sheet and update the word document with the data provided in the excel sheet. Save the updated word document in the local directory. The updated Word documents are then converted into PDF. This PDF files are sent as a attachment to the email address provided in the excel sheet.",
     LogInTitle:"LOGIN",
     LoginArray:["Provide user name, password","Provide the SMTP/POP server name from which domain you are sending (servername: send.one.com and port number: 587(depends on the domain provider))","Make sure to check “Check label” not to repeat the details. If the details repeat close and run the application again"],
